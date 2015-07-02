d5396e1c9d0c6983cbd865f6170b756216984f15
ekamoto <ekamoto.leandro@gmail.com>
2015-07-02 17:35:04 -0400

    Correção para vazamento de memória: OutOfMemoryError

--- src/com/example/minhabiblioteca/adapter/LivroAdapter.java
+++ src/com/example/minhabiblioteca/adapter/LivroAdapter.java
@@ -1,7 +1,11 @@
 package com.example.minhabiblioteca.adapter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import android.graphics.Bitmap;
 import com.example.minhabiblioteca.R;
 import com.example.minhabiblioteca.model.Livro;
 
@@ -39,23 +43,57 @@ public class LivroAdapter extends BaseAdapter {
 		return position;
 	}
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+    private Bitmap decodeFile(File f) {
+        try {
 
-		Livro livro = livros.get(position);
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f),null,o);
 
-		LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View view = inflater.inflate(R.layout.lista_livros, null);
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=70;
 
-		TextView txttitulo = (TextView) view.findViewById(R.id.txtTituloLivro);
-		txttitulo.setText(livro.getTitulo());
+            //Find the correct scale value. It should be the power of 2.
+            int scale=1;
+            while(o.outWidth/scale/2>=REQUIRED_SIZE && o.outHeight/scale/2>=REQUIRED_SIZE)
+                scale*=2;
 
-		ImageView imagemLivro = (ImageView) view.findViewById(R.id.imagemLivro);
-		imagemLivro.setImageBitmap(BitmapFactory.decodeFile(livro
-				.getPathImagem()));
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize=scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {}
+        return null;
+    }
 
-		return view;
-	}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Livro livro = livros.get(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // Não inflar toda hora
+        if (convertView == null) {
+
+            convertView = mInflater.inflate(R.layout.lista_livros, parent, false);
+        }
+
+        TextView txttitulo = (TextView) convertView.findViewById(R.id.txtTituloLivro);
+        txttitulo.setText(livro.getTitulo());
+
+        ImageView imagemLivro = (ImageView) convertView.findViewById(R.id.imagemLivro);
+
+        // Desse jeito fica muito pesado a imagem para uma ListView
+        // Bitmap bitmap = decodeFile(new File(livro.getPathImagem()));
+        // imagemLivro.setImageBitmap(bitmap);
+
+        Bitmap bitmap = decodeFile(new File(livro.getPathImagem()));
+        imagemLivro.setImageBitmap(bitmap);
+
+        return convertView;
+    }
 
 }
--- src/com/example/minhabiblioteca/ui/DetalheItemActivity.java
+++ src/com/example/minhabiblioteca/ui/DetalheItemActivity.java
@@ -3,6 +3,7 @@ package com.example.minhabiblioteca.ui;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,12 +11,14 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.TextView;
+import android.widget.*;
 
 import com.example.minhabiblioteca.R;
 import com.example.minhabiblioteca.model.Livro;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class DetalheItemActivity extends Activity {
 
@@ -25,6 +28,7 @@ public class DetalheItemActivity extends Activity {
     private TextView txtResumo;
     private RatingBar avalicoesLivro;
     private Livro livro;
+    private Bitmap img;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @SuppressLint("UseValueOf")
@@ -41,7 +45,9 @@ public class DetalheItemActivity extends Activity {
         txtResumo = (TextView) findViewById(R.id.txtResumo);
         avalicoesLivro = (RatingBar) findViewById(R.id.avaliacaoLivro);
 
-        imagemLivro.setImageBitmap(BitmapFactory.decodeFile(livro.getPathImagem()));
+        img = BitmapFactory.decodeFile(livro.getPathImagem());
+        imagemLivro.setImageBitmap(img);
+
         txtTitulo.setText(livro.getTitulo());
         txtAutor.setText(livro.getAutor());
         txtResumo.setText(livro.getResumo());
@@ -49,6 +55,40 @@ public class DetalheItemActivity extends Activity {
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        imagemLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(img.getHeight(), img.getWidth());
+
+                imagemLivro.setLayoutParams(layout);
+            }
+        });
+
+    }
+
+    private Bitmap decodeFile(File f){
+        try {
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f),null,o);
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=70;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale=1;
+            while(o.outWidth/scale/2>=REQUIRED_SIZE && o.outHeight/scale/2>=REQUIRED_SIZE)
+                scale*=2;
+
+            //Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize=scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {}
+        return null;
     }
 
     @Override
